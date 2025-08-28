627e73ce14198bbebaadfe5f5a963f276c79a37b
Sanjay <sanjaydevarajworks@gmail.com>
2025-08-28 23:31:08 +0530

    Create an home page

--- CarWorld/settings.py
+++ CarWorld/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'CarWorld.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
